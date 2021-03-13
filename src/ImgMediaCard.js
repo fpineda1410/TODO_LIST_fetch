1f3abed16123dedc4514765cb1e655580bb05f6c
fpineda1410 <fpineda147@outlook.com>
2021-03-12 20:33:06 -0600

    API initial request solved

--- src/ImgMediaCard.js
+++ src/ImgMediaCard.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect,useCallback} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
@@ -16,7 +16,9 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import { v4 as uuidv4 } from "uuid";
+import axios from 'axios';
 
+var inner_data=[];
 
 const useStyles = makeStyles({
   root: {
@@ -29,20 +31,82 @@ const content = {
   inputLabel: "Todo List"
 }
 
+// const fetchData = async () => {
+  
+//   fetch('https://assets.breatheco.de/apis/fake/todos/user/fpineda1410') //Generic GET Method to the 4Geeks API
+//                   .then(response => response.json())
+//                   .then(data => {
+//                     data.forEach((item)=> inner_data.push({name:item.label, id: uuidv4()}))
+                    
+//                     console.log(inner_data)
+                    
+                  
+//                   })
+        
+                    
+          
+//   };
+
+  //fetchData();
+
+  //console.log(inner_data);
+  /*list.forEach((item)=>{ //translates the local List to the requirements of the API
+                        json_list.push({"label": item.name, "done":false});                    }); */
+
+//fetchData();
+
+
 //Initial list of activities in order to useState
+const initialList = inner_data;
+
+/* 
 const initialList = [
   {
-    id: "a",
-    name: "wash the dishes",
+    label: "wash the dishes",
+    done: "false",
   }
 
 ];
+hacer una funcion global json list vacia que se actualice solo con el fetch
+
+const json_List =[];
+
+
+
+*/ 
+
+
 
 
  function ImgMediaCard() {
+  
+  
+
+
   const classes = useStyles();
-  const [list, setList] = useState(initialList);
+
+ //const initialList = fetchData(inner_data);
+ 
+  
+  const [list, setList] = useState([]); //initiallist //lo primero que tiene que hacer no es establecer este use state pero el contenido del json
   const [name, setName] = useState('');
+        //setList(()=>fetchData());
+
+        // const fetchData = useCallback((inner_data1) => {
+  
+        //   fetch('https://assets.breatheco.de/apis/fake/todos/user/fpineda1410') //Generic GET Method to the 4Geeks API
+        //                   .then(response => response.json())
+        //                   .then(data => {
+        //                     data.forEach((item)=> inner_data1.push({name:item.label, id: uuidv4()}))
+                            
+        //                     setList(inner_data1);
+                            
+                          
+        //                   })
+                
+                            
+                  
+        //   },[]);
  
         function handleChange(event) {
           setName(event.target.value);
@@ -50,16 +114,17 @@ const initialList = [
         }
       
         function handleAdd() {
-          const newList = list.concat({name, id: uuidv4()});
+          const newList = list.concat({name, id: uuidv4()}); //false en lugar de id
           setList(newList);
-
+          console.log(newList);
           setName('');
         }
 
         function handleRemove(id) {
-          const newList = list.filter((item) => item.id !== id);
+          const newList = list.filter((item) => item.id !== id);//item.label
       
           setList(newList);
+         
         }
 
         function refresh() {
@@ -70,8 +135,21 @@ const initialList = [
 
         }
 
-              useEffect (()=>{
+              
 
+              useEffect (()=>{
+                fetch('https://assets.breatheco.de/apis/fake/todos/user/fpineda1410') //Generic GET Method to the 4Geeks API
+                  .then(response => response.json())
+                  .then(data => {console.log(data)
+                    if (list.length === 0){
+                    
+                      data.forEach((item)=> inner_data.push({name:item.label, id: uuidv4()}))
+                    
+                      setList(inner_data)
+                    
+                    }
+                  
+                  })
                 //Put request when List changes
                 function postRequest(url, list) {//data would be newlist
                     const json_list = [];
@@ -79,7 +157,7 @@ const initialList = [
                       list.forEach((item)=>{ //translates the local List to the requirements of the API
                         json_list.push({"label": item.name, "done":false});
                       });
-                    }else {
+                    }else if (list.length === 0){
                       json_list.push({"label": 'empty', "done":false}); //Generic Value!
                     }
                     return fetch(url, {
@@ -92,18 +170,26 @@ const initialList = [
                     })
                     .then(response => response.json())
                     .then (data=>{
-                        console.log(data);
+                        //console.log(data);
                     })
                 }
             //-------------------------------Synchronization with the API 
                 async function Put_Get () {
+
+                  
                   await postRequest('https://assets.breatheco.de/apis/fake/todos/user/fpineda1410', list)
                   .then(data => console.log(data)) // Result from the `response.json()` call
                   .catch(error => console.error(error))
 
                   fetch('https://assets.breatheco.de/apis/fake/todos/user/fpineda1410') //Generic GET Method to the 4Geeks API
                   .then(response => response.json())
-                  .then(data => console.log(data))
+                  .then(data => {//console.log(data)
+                    
+                  }
+                  )
+
+                  
+                   //setJsonList(data)
                 }
             //-------------------------------END OF Synchronization with the API 
 
@@ -134,7 +220,7 @@ const initialList = [
                 onRefresh={refresh}
                 />
 
-                <ListGenerator list = {list} onRemove={handleRemove}/>
+                <ListGenerator list = {list} onRemove={handleRemove}/> {/*aqui el lee el json list*/}
               
               </CardContent>
             
@@ -151,6 +237,7 @@ const initialList = [
 //------------------------------------------------------Verifiers in order to update the values inserted into the MAIN
 const ListGenerator = ({ list,onRemove }) => {
       
+  
   return list.map((item) => (
     <ListItem key={item.id}>
       <ListItemText primary={item.name} />
@@ -159,6 +246,7 @@ const ListGenerator = ({ list,onRemove }) => {
       </Button>
     </ListItem>
   ));
+
 }
   
 
